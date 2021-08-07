8e32a9d6dc7276f1cd7060fc3110e0866179a29c
Andrei Trybukhouski <t.andrey@tut.by>
2021-08-08 00:08:16 +0300

    intermediate

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -1,26 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-  required_version = ">= 0.14.9"
-  backend "s3" {
-    bucket = "andrei-trybukhouski-test-project-terraform"
-    key    = "test/terraform.tfstate"
-    region = "eu-central-1"
-  }
-}
-
-provider "aws" {
-  profile = "default"
-  region  = "eu-central-1"
-
-}
-
-
-
 data "aws_ami" "latest_ubuntu" {
   owners      = ["099720109477"]
   most_recent = true
@@ -51,6 +28,13 @@ data "aws_ssm_parameter" "dev_pass" {
     aws_ssm_parameter.dev_pass
   ]
 }
+
+resource "aws_eip" "lb" {
+  instance = aws_instance.app_server[0].id
+  vpc      = true
+
+}
+
 
 resource "aws_instance" "app_server" {
   count                  = var.serverscount
